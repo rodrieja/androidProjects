8392b48346ab0a5f5da51178e70f627757e86649
DELL-INSPIRON\rodri <rodriguezalejandro.e@gmail.com>
2017-09-20 10:26:17 -0300

    Envío de Emails desde GMail

--- DetectorCaidas/app/src/main/java/tecnica/a7mo/elizalde/detectorcaidas/MainActivity.java
+++ DetectorCaidas/app/src/main/java/tecnica/a7mo/elizalde/detectorcaidas/MainActivity.java
@@ -1,12 +1,17 @@
 package tecnica.a7mo.elizalde.detectorcaidas;
 
+import android.app.Activity;
+import android.content.Intent;
 import android.graphics.Color;
 import android.hardware.Sensor;
 import android.hardware.SensorEvent;
 import android.hardware.SensorEventListener;
 import android.hardware.SensorManager;
+import android.net.Uri;
+import android.os.StrictMode;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.view.Window;
 import android.view.WindowManager;
@@ -26,6 +31,13 @@ public class MainActivity extends AppCompatActivity implements SensorEventListen
 
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        if (android.os.Build.VERSION.SDK_INT > 9)
+        {
+            StrictMode.ThreadPolicy policy = new StrictMode.ThreadPolicy.Builder().permitAll().build();
+            StrictMode.setThreadPolicy(policy);
+        }
+
         view = findViewById(R.id.textView);
         view.setBackgroundColor(Color.GREEN);
 
@@ -40,7 +52,6 @@ public class MainActivity extends AppCompatActivity implements SensorEventListen
         if (event.sensor.getType() == Sensor.TYPE_ACCELEROMETER) {
             getAccelerometer(event);
         }
-
     }
 
     private void getAccelerometer(SensorEvent event) {
@@ -68,6 +79,8 @@ public class MainActivity extends AppCompatActivity implements SensorEventListen
                 view.setBackgroundColor(Color.RED);
             }
             color = !color;
+
+            sendEmail();
         }
     }
 
@@ -92,4 +105,32 @@ public class MainActivity extends AppCompatActivity implements SensorEventListen
         super.onPause();
         sensorManager.unregisterListener(this);
     }
+
+    private void sendEmail() {
+        try {
+            GMailSender sender = new GMailSender("from@gmail.com", "pass");
+            sender.sendMail("This is Subject",
+                    "This is Body",
+                    "from@gmail.com",
+                    "to@gmail.com");
+        } catch (Exception e) {
+            Log.e("SendMail", e.getMessage(), e);
+        }
+    }
+    /*
+    private void sendEmail(){
+
+        Intent emailIntent = new Intent(Intent.ACTION_SENDTO);
+        emailIntent.setData(Uri.parse("mailto:" + "recipient@example.com"));
+        emailIntent.putExtra(Intent.EXTRA_SUBJECT, "My email's subject");
+        emailIntent.putExtra(Intent.EXTRA_TEXT, "My email's body");
+
+        try {
+            startActivity(Intent.createChooser(emailIntent, "Send email using..."));
+        } catch (android.content.ActivityNotFoundException ex) {
+            Toast.makeText(MainActivity.this, "No email clients installed.", Toast.LENGTH_SHORT).show();
+        }
+
+    }
+    */
 }
